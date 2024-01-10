beca925f946efbac39284a95c26e88dbaa1404d2
iamvishal16 <34365396+iAmVishal16@users.noreply.github.com>
2024-01-10 23:07:43 +0530

    Initial Commit

---
+++ MicroExp/MicroExpApp.swift
@@ -0,0 +1,17 @@
+//
+//  MicroExpApp.swift
+//  MicroExp
+//
+//  Created by Vishal Paliwal on 10/01/24.
+//
+
+import SwiftUI
+
+@main
+struct MicroExpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
